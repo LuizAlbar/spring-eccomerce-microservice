7f0b3f1abc0c786186fe36241d802f729b925a4d
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-04-15 16:24:21 -0300

    feat: add update user route

--- src/main/java/com/app/ecom/UserService.java
+++ src/main/java/com/app/ecom/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,18 +16,27 @@ public class UserService {
     public List<User> fetchAllUsers() {
         return userList;
     }
+
     public void addUser(User user) {
         user.setId(nextId++);
         userList.add(user);
 
     }
 
-    public User fetchUser(Long id) {
-        for(User user: userList) {
-            if(user.getId().equals(id)) {
-                return user;
-            }
-        }
-        return null;
+    public Optional<User> fetchUser(Long id) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+    }
+
+    public boolean updateUser(Long id, User updatedUser) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .map(existingUser -> {
+                    existingUser.setFirstName(updatedUser.getFirstName());
+                    existingUser.setLastName(updatedUser.getLastName());
+                    return true;
+                }).orElse(false);
     }
 }
