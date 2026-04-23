be0ac62f775df435e19f917257656353d7978314
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-04-22 21:45:54 -0300

    feat: implement dto pattern

--- src/main/java/com/app/ecom/service/UserService.java
+++ src/main/java/com/app/ecom/service/UserService.java
@@ -1,5 +1,9 @@
 package com.app.ecom.service;
 
+import com.app.ecom.dto.AddressDTO;
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
+import com.app.ecom.model.Address;
 import com.app.ecom.repository.UserRepository;
 import com.app.ecom.model.User;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,19 +22,24 @@ public class UserService {
 //    private Long nextId = 1L;
 
 
-    public List<User> fetchAllUsers() {
+    public List<UserResponse> fetchAllUsers() {
 
-        return userRepository.findAll();
+        return userRepository.findAll()
+                .stream()
+                .map(this::mapToUserResponse)
+                .collect(Collectors.toList());
     }
 
-    public void addUser(User user) {
+    public void addUser(UserRequest userRequest) {
         //user.setId(nextId++);
+        User user  = new User();
+        updateUserFromRequest(user, userRequest);
         userRepository.save(user);
 
     }
 
-    public Optional<User> fetchUser(Long id) {
-        return userRepository.findById(id);
+    public Optional<UserResponse> fetchUser(Long id) {
+        return userRepository.findById(id).map(this::mapToUserResponse);
     }
 
     public boolean updateUser(Long id, User updatedUser) {
@@ -40,5 +50,44 @@ public class UserService {
                     userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
+    }
+
+    private UserResponse mapToUserResponse(User user) {
+        UserResponse response = new UserResponse();
+        response.setId(user.getId());
+        response.setFirstName(user.getFirstName());
+        response.setLastName(user.getLastName());
+        response.setEmail(user.getEmail());
+        response.setPhone(user.getPhone());
+        response.setRole(user.getRole());
+
+        if (user.getAddress() != null) {
+            Address userAddress = user.getAddress();
+            AddressDTO addressDTO = new AddressDTO();
+            addressDTO.setStreet(userAddress.getStreet());
+            addressDTO.setCity(userAddress.getCity());
+            addressDTO.setState(userAddress.getState());
+            addressDTO.setCountry(userAddress.getCountry());
+            addressDTO.setZipcode(userAddress.getZipcode());
+            response.setAddress(addressDTO);
+        }
+
+        return response;
+    }
+
+    private void updateUserFromRequest(User user, UserRequest userRequest){
+        user.setFirstName(userRequest.getFirstName());
+        user.setLastName(userRequest.getLastName());
+        user.setEmail(userRequest.getEmail());
+        user.setPhone(userRequest.getPhone());
+        if (userRequest.getAddress() != null) {
+            Address address = new Address();
+            address.setStreet(userRequest.getAddress().getStreet());
+            address.setCity(userRequest.getAddress().getCity());
+            address.setState(userRequest.getAddress().getState());
+            address.setCountry(userRequest.getAddress().getCountry());
+            address.setZipcode(userRequest.getAddress().getZipcode());
+            user.setAddress(address);
+        }
     }
 }
