656c3ef986d001ed71b00918185b261bc9b5a611
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-04-28 19:46:41 -0300

    feat: add cart controller

---
+++ src/main/java/com/app/ecom/controller/CartController.java
@@ -0,0 +1,38 @@
+package com.app.ecom.controller;
+
+
+import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<String> addToCart(
+            @RequestHeader("X-User-ID") String userId,
+            @RequestBody CartItemRequest request
+    ) {
+        if(!cartService.addToCart(userId, request)) {
+            return ResponseEntity.badRequest().body("Product Out of Stock or User/Product Not Found");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/items/{productId}")
+    public ResponseEntity<Void> removeFromCart(
+            @RequestHeader("X-User-ID") String userId,
+            @PathVariable Long productId
+    ){
+        boolean deleted = cartService.deleteItemFromCart(userId, productId);
+
+        return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
+}
